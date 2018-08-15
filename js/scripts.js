c8d625fd25ed47c3e9e974c0363f830fd416fe6d
Akjol Jaenbai and Brian Nelson <akjol@>
2018-08-15 10:52:32 -0700

    add function to move first group of consonants to end of word and add ay

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 var convertToPigLatin = function(txt) {
   // var vowels = ["a", "e", "i", "o", "u"];
   var vowels = "aeiou";
+  var consonants = "bcdfghjklmnpqrstvwxyz";
   var output = '';
 
 
     if (vowels.includes(txt[0])) {
-
       output = txt + "ay";
+    } else if (consonants.includes(txt[0])) {
+      for(i=0;i<txt.length;i++) {
+        if (consonants.includes(txt[i])) {
+        } else {
+
+          var first = txt.substr(0,i);
+          var rest = txt.substr(i,txt.length);
+
+          output = rest + first + "ay";
+          i = txt.length;
+        }
+      }
     } else if (/[^0-9]/gi.test(txt)) {
       return "is text";
     } else {
